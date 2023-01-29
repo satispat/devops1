0aade098f47c5b1b8a8b498a7613b4e3630822be
satispat <30749310+satispat@users.noreply.github.com>
2023-01-29 13:36:45 +0530

    Update GetDao.java

--- java-tomcat-sample/src/main/java/dao/GetDao.java
+++ java-tomcat-sample/src/main/java/dao/GetDao.java
@@ -19,10 +19,10 @@ public class GetDao
 			//Connection con = DriverManager.getConnection("jdbc:mysql://database-mysql.cidzsb0w1shy.us-east-1.rds.amazonaws.com:3306/db","admin","Awsdevop123");
 			// Connection con = DriverManager.getConnection("jdbc:mysql://DB_HOSTNAME_HERE:3306/db","root","DB_PASSWORD_HERE");
 			
-				db_username=java ssm.getparameter('dev_rds_creds')
-				db_password=java ssm.getparameter('dev_rds_creds')
-				Replace the code to retrieve the db_hostname, db_user, db_password from SSM Parameter Store
-				Replace the code to retrieve the db_hostname, db_user, db_password from Secret Manager
+			db_username=java ssm.getparameter('dev_rds_creds');
+			db_password=java ssm.getparameter('dev_rds_creds');
+				//Replace the code to retrieve the db_hostname, db_user, db_password from SSM Parameter Store
+				//Replace the code to retrieve the db_hostname, db_user, db_password from Secret Manager
 			Connection con = DriverManager.getConnection("jdbc:mysql://database-mysql.cidzsb0w1shy.us-east-1.rds.amazonaws.com:3306/db",db_username,db_password);
 			
 			Statement st = con.createStatement();
